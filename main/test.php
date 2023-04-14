5593fd9011ec3c4c16c67f61c96052f285c43f0e
mohammed-ask <mohammedmaheswer12@gmail.com>
2023-04-14 22:19:44 +0530

    Update test.php

--- main/test.php
+++ main/test.php
@@ -2,5 +2,36 @@
 include 'main/session.php';
 // echo $obj->getrequesttoken();
 // echo $obj->getaccesstoken();
-echo $obj->getcandledata();
+// echo $obj->getcandledata();
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'main/phpmailer/src/Exception.php';
+require 'main/phpmailer/src/PHPMailer.php';
+require 'main/phpmailer/src/SMTP.php';
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->Host = $host;
+$mail->SMTPAuth = $smtpauth;
+$mail->Username = "$sendmailfrom";
+$mail->Password = "$sendemailpassword";
+if ($issmtp) {
+    $mail->SMTPSecure = 'ssl';
+}
+$mail->Port = $port;
+$mail->setFrom("$sendmailfrom");
+$mail->addAddress('mohammedmaheswer12@gmail.com');
+$mail->isHTML(true);
+$mail->Subject = 'yoyoyo';
+// $attachfile = $obj->selectextrawhere('maildocuments', "mailid=$mailid");
+// while ($rowfile = $obj->fetch_assoc($attachfile)) {
+//     $path = $obj->selectfieldwhere('uploadfile', "path", "id=" . $rowfile['path'] . "");
+//     $orgname = $obj->selectfieldwhere('uploadfile', "orgname", "id=" . $rowfile['path'] . "");
+//     $mail->addAttachment($path, $orgname);
+// }
+$mail->Body = 'this is bboady';
+$mail->send();
+
 echo 'runnpage';
