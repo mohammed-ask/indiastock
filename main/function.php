97cc61d637153b9210d9eecb52c196f80ff37044
mohammed-ask <mohammedmaheswer12@gmail.com>
2023-04-23 20:58:57 +0530

    Update function.php

--- main/function.php
+++ main/function.php
@@ -1498,7 +1498,7 @@ class db
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
         $response = curl_exec($ch);
         $result = json_decode($response, true);
         var_dump($result);
