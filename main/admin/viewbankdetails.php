87a82afcf9de52cf0a9bef385872d46b37ce23e7
codinmonks <codinmonks@gmail.com>
2023-04-25 14:37:45 +0530

    uppercase removed

--- main/admin/viewbankdetails.php
+++ main/admin/viewbankdetails.php
@@ -5,7 +5,7 @@ $rowbank = $obj->selectextrawhereupdate("users", "bankname,accountno,ifsc", "id=
 ?>
 <div class="px-3 bg-white rounded-lg ">
     <span>
-        <p class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray"><b>Bank Name: </b><?= $rowbank['bankname'] ?> <br><span style="margin-"> <b>A/c No.: </b></span><?= $rowbank['accountno'] ?> <br><b>IFSC: </b><?= $rowbank['ifsc'] ?></p>
+        <p class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray"><b>Bank Name: </b><?= $rowbank['bankname'] ?> <br><span style="margin-top: 10px !important; margin-bottom: 10px !important;"> <b>A/c No: </b></span><?= $rowbank['accountno'] ?> <br><b>IFSC: </b><?= $rowbank['ifsc'] ?></p>
 
     </span>
 </div>
