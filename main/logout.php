1941cbad88996eea8bc43290ca2026c7f78471f4
mohammed-ask <mohammedmaheswer12@gmail.com>
2023-04-27 23:46:33 +0530

    Update logout.php

--- main/logout.php
+++ main/logout.php
@@ -4,7 +4,7 @@ $head = "login";
 if (($_SERVER['HTTP_HOST'] == 'localhost')) {
     $head = "/indiastock";
 }
-if (str_contains($_SERVER['REQUEST_URI'], "$head/admin")) {
+if (str_contains($_SERVER['REQUEST_URI'], "admin")) {
     $head = 'admin/adminlogin';
 }
 $index =  $head;
