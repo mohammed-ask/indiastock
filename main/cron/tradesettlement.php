823ffdbf52d9a430b4283ff550e2bba16644fe41
mohammed-ask <mohammedmaheswer12@gmail.com>
2023-04-30 22:31:27 +0530

    Update tradesettlement.php

--- main/cron/tradesettlement.php
+++ main/cron/tradesettlement.php
@@ -304,7 +304,7 @@ while ($row = $obj->fetch_assoc($result)) {
         }
     });
     $keys = array_keys($pricedata)[0];
-    $currentrate = $pricedata[$keys]['LastRate'];
+    $currentrate = 35; //$pricedata[$keys]['LastRate'];
     $xc['added_on'] = date("Y-m-d H:i:s");
     $xc['updated_on'] = date("Y-m-d H:i:s");
     $xc['status'] = 1;
