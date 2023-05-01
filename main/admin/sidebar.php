551c95365fc51a2ba178355ce2fd3cdd3693df39
mohammed-ask <mohammedmaheswer12@gmail.com>
2023-05-02 01:24:35 +0530

    sidebar

--- main/admin/sidebar.php
+++ main/admin/sidebar.php
@@ -430,49 +430,15 @@
           </li>
         </ul>
       <?php } ?>
-      <?php if (in_array(25, $permissions) || in_array(26, $permissions) || in_array(27, $permissions) || in_array(28, $permissions) || in_array(29, $permissions)) { ?>
-        <li class="relative px-6 py-3">
-          <button class="inline-flex items-center justify-between w-full  font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" @click="toggleInvestmentPageMenu" aria-haspopup="true">
-            <span class="inline-flex items-center">
-              <span class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                <i style="color: #057c7c;" class="fa-solid fa-indian-rupee-sign"></i>
-              </span>
-              <span class="ml-3">Investment</span>
-            </span>
-            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-            </svg>
-          </button>
-          <template x-if="isInvestmentPageMenuOpen">
-            <ul x-transition:enter="transition-all ease-in-out duration-300" x-transition:enter-start="opacity-25 max-h-0" x-transition:enter-end="opacity-100 max-h-xl" x-transition:leave="transition-all ease-in-out duration-300" x-transition:leave-start="opacity-100 max-h-xl" x-transition:leave-end="opacity-0 max-h-0" class="p-2 mt-2 space-y-2 overflow-hidden  font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900" aria-label="submenu">
-              <?php if (in_array(25, $permissions)) { ?>
-                <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="allinvestment">All</a>
-                </li>
-              <?php } ?>
-              <?php if (in_array(26, $permissions)) { ?>
-                <!-- <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="pendinginvestment">Pending</a>
-                </li> -->
-              <?php } ?>
-              <?php if (in_array(27, $permissions)) { ?>
-                <!-- <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="approvedinvestment">Approved</a>
-                </li> -->
-              <?php } ?>
-              <?php if (in_array(28, $permissions)) { ?>
-                <!-- <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="disapprovedinvestment">Disapproved</a>
-                </li> -->
-              <?php } ?>
-              <?php if (in_array(29, $permissions)) { ?>
-                <!-- <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="withdrawalrequest">Withdrawal Requests</a>
-                </li> -->
-              <?php } ?>
-            </ul>
-          </template>
-        </li>
+      <?php if (in_array(25, $permissions)) { ?>
+        <ul>
+          <li class="relative px-6 py-3">
+            <a class="inline-flex items-center w-full  font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="allinvestment">
+              <i style="color: #057c7c;" class="fa-solid fa-indian-rupee-sign"></i>
+              <span class="ml-4">Investment</span>
+            </a>
+          </li>
+        </ul>
       <?php } ?>
       <?php if (in_array(29, $permissions)) { ?>
         <ul>
