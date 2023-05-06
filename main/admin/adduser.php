73fda5d9088d45116b9f2e1d102f9e6f3b84f942
singhshubhu <singhatish205@gmail.com>
2023-05-06 14:11:11 +0530

    add-edit user

--- main/admin/adduser.php
+++ main/admin/adduser.php
@@ -7,72 +7,83 @@
         <span class="text-gray-700 dark:text-gray-400">Email</span>
         <input name="email" data-bvalidator="required,email" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's Email ID" />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Mob No.</span>
         <input data-bvalidator="required,digit,minlength[10],maxlength[10]" name="mobileno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's Mobile No." />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">DOB</span>
-        <input id="date" data-bvalidator="required,gap18year" onfocus="setcalenderlimit(this.id,'')" data-bvalidator-msg-gap18year="Customer Should be minimum 18 year Old" name="dob" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Date of Birth" /></label>
+        <input id="date" data-bvalidator="required,gap18year" onfocus="setcalenderlimit(this.id,'')" data-bvalidator-msg-gap18year="Customer Should be minimum 18 year Old" name="dob" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Date of Birth" /></label> </div>
     <label class="block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Address</span>
         <input data-bvalidator="required" name="address" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's Address" />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
-        <span class="text-gray-700 dark:text-gray-400">Aadhar No.</span>
-        <input data-bvalidator="required" name="adharno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Enter 12 Digit Aadhar No." /></label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
-        <span class="text-gray-700 dark:text-gray-400">PAN No.</span>
-        <input data-bvalidator="required" name="panno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's Pan No." /></label>
 
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
+        <span class="text-gray-700 dark:text-gray-400">Aadhar No.</span>
+        <input data-bvalidator="required" name="adharno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Enter Aadhar No." /></label>
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
+        <span class="text-gray-700 dark:text-gray-400">PAN No.</span>
+        <input data-bvalidator="required" name="panno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's Pan No." /></label></div>
+        <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Bank Name</span>
         <input data-bvalidator="required" name="bankname" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="eg. BOI, State bank of India, Kotak etc..." /></label>
 
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Account No.</span>
-        <input data-bvalidator="required" name="accountno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's A/c No." /></label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
-        <span class="text-gray-700 dark:text-gray-400">IFSC Code</span>
+        <input data-bvalidator="required" name="accountno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's A/c No." /></label> </div>
+
+        <div class="row">
+
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
+        <span class="text-gray-700 dark:text-gray-400">IFSC</span>
         <input data-bvalidator="required" name="ifsc" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="IFSC Code of Bank" /></label>
 
     <!-- <label class="block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Investment Amt.</span>
         <input name="investmentamount" value="0" data-bvalidator="required,digit" step="any" onfocus="this.select()" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Client's Investment" /></label> -->
-        <div class="row">
+        
     <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Limit</span>
-        <input type="number" name="limit" data-bvalidator="required" step="any" onfocus="this.select()" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="1" placeholder="Client's Limit on Investment" /></label>
+        <input type="number" name="limit" data-bvalidator="required" step="any" onfocus="this.select()" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="1" placeholder="Client's Limit on Investment" /></label></div>
+
+        <div class="row">
     <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Stop Withdrawal- <span style="margin-left: 5px !important;">From</span></span>
         <input name="starttime" id="starttime" onfocus="datetimepicker(this.id)" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Select Start Time" />
-    </label></div>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    </label>
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">To</span>
         <input name="endtime" id="endtime" onfocus="datetimepicker(this.id)" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Select End Time" />
-    </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    </label></div>
+
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Password</span>
-        <input type="password" data-bvalidator="required" id="password" name="password" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Please Give Strong Password!" />
+        <input type="password" data-bvalidator="required" id="password" name="password" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Enter Password!" />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Confirm Password</span>
         <input type="password" id="confirmpassword" data-bvalidator="required,matchconfirmpassword[password]" data-bvalidator-msg-matchconfirmpassword="Confirm Password Not Matched" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Confirm Password" />
-    </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    </label></div>
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Carry Forward</span>
         <select data-bvalidator="required" name="carryforward" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
             <option value="No">No</option>
             <option value="Yes">Yes</option>
         </select>
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Long Holding</span>
         <select data-bvalidator="required" name="longholding" class="select2 block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
             <option value="No">No</option>
             <option value="Yes">Yes</option>
         </select>
-    </label>
+    </label></div>
     <label class="block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Employee ID</span>
         <input xdata-bvalidator="required" name="employeeref" class="select2 block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Employee ID For Furthur Reference" /></label>
--- main/admin/edituser.php
+++ main/admin/edituser.php
@@ -12,41 +12,46 @@ $rowuser = $obj->selectextrawhere('users', 'id="' . $id . '"')->fetch_assoc();
         <span class="text-gray-700 dark:text-gray-400">Email</span>
         <input name="email" data-bvalidator="required,email" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['email'] ?>" placeholder="Client's Email ID" />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Mob No.</span>
         <input data-bvalidator="required,digit,minlength[10],maxlength[10]" name="mobileno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['mobile'] ?>" placeholder="Client's Mobile No." />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">DOB</span>
-        <input id="date" data-bvalidator="required,gap18year" onfocus="setcalenderlimit(this.id,'')" data-bvalidator-msg-gap18year="Customer Should be minimum 18 year Old" name="dob" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= changedateformatespecito($rowuser['dob'], "Y-m-d", "d/m/Y") ?>" placeholder="Date of Birth" /></label>
+        <input id="date" data-bvalidator="required,gap18year" onfocus="setcalenderlimit(this.id,'')" data-bvalidator-msg-gap18year="Customer Should be minimum 18 year Old" name="dob" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= changedateformatespecito($rowuser['dob'], "Y-m-d", "d/m/Y") ?>" placeholder="Date of Birth" /></label></div>
     <label class="block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Address</span>
         <input data-bvalidator="required" name="address" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['address'] ?>" placeholder="Client's Address" />
     </label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Aadhar No.</span>
         <input data-bvalidator="required" name="adharno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['adharno'] ?>" placeholder="Enter 12 Digit Aadhar No." /></label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">PAN No.</span>
-        <input data-bvalidator="required" name="panno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['panno'] ?>" placeholder="Client's Pan No." /></label>
+        <input data-bvalidator="required" name="panno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['panno'] ?>" placeholder="Client's Pan No." /></label></div>
 
-    <label class="block text-sm" style="margin-bottom: 5px;">
+        <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Bank Name</span>
         <input data-bvalidator="required" name="bankname" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['bankname'] ?>" placeholder="eg. BOI, State bank of India, Kotak etc..." /></label>
 
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Account No.</span>
-        <input data-bvalidator="required" name="accountno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['accountno'] ?>" placeholder="Client's A/c No." /></label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+        <input data-bvalidator="required" name="accountno" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['accountno'] ?>" placeholder="Client's A/c No." /></label></div>
+
+        <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">IFSC Code</span>
         <input data-bvalidator="required" name="ifsc" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['ifsc'] ?>" placeholder="IFSC Code of Bank" /></label>
 
-    <label class="block text-sm" style="margin-bottom: 5px;">
-        <span class="text-gray-700 dark:text-gray-400">Investment Amt.</span>
-        <input type="number" name="investmentamount" data-bvalidator="required" step="any" onfocus="this.select()" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['investmentamount'] ?>" readonly placeholder="Client's Investment" /></label>
-    <label class="block text-sm" style="margin-bottom: 5px;">
+    
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Limit</span>
-        <input type="number" name="limit" data-bvalidator="required" step="any" onfocus="this.select()" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['limit'] ?>" placeholder="Client's Limit on Investment" /></label>
+        <input type="number" name="limit" data-bvalidator="required" step="any" onfocus="this.select()" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" value="<?= $rowuser['limit'] ?>" placeholder="Limit on Investment" /></label></div>
         <div class="row">
     <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Stop Withdrawal- <span style="margin-left: 5px !important;">From</span></span>
@@ -82,18 +87,17 @@ $rowuser = $obj->selectextrawhere('users', 'id="' . $id . '"')->fetch_assoc();
             ?>
         </select>
     </label><br> -->
-    <label class="block text-sm" style="margin-bottom: 5px;position:relative">
+
+    <div class="row">
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;position:relative">
         <span class="text-gray-700 dark:text-gray-400">Change Password</span>
         <input type="password" id="password" value="<?= $rowuser['password'] ?>" name="password" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Please Give Strong Password!" />
-        <i id="eye" class="fa fa-eye" style="position: absolute;top:34px;right:10px;z-index:50" aria-hidden="true"></i>
+        <i id="eye" class="fa fa-eye" style="position: absolute;top:34px;right:12px;z-index:50" aria-hidden="true"></i>
     </label>
-    <!-- <label class="block text-sm" style="margin-bottom: 5px;">
-        <span class="text-gray-700 dark:text-gray-400">Confirm Password</span>
-        <input type="password" id="confirmpassword" data-bvalidator="matchconfirmpassword[password]" data-bvalidator-msg-matchconfirmpassword="Confirm Password Not Matched" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Confirm Password" />
-    </label> -->
-    <label class="block text-sm" style="margin-bottom: 5px;">
+   
+    <label class="col-6 block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Employee ID</span>
-        <input xdata-bvalidator="required" name="employeeref" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" disabled value="<?= $rowuser['employeeref'] === '' ? 'NA' : $rowuser['employeeref'] ?>" placeholder="Employee ID For Further Reference" /></label>
+        <input xdata-bvalidator="required" name="employeeref" class="block w-full  text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" disabled value="<?= $rowuser['employeeref'] === '' ? 'NA' : $rowuser['employeeref'] ?>" placeholder="Employee ID For Further Reference" /></label></div>
     <div>
         <button type="submit" id="modalsubmit" class="w-full px-3 py-1 mt-6 text-sm font-medium hidden">
             Submit
