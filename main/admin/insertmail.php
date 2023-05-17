9a38aa387b0f2d710dc7f6af54706279657054cf
mohammed-ask <mohammedmaheswer12@gmail.com>
2023-05-17 23:29:47 +0530

    Update insertmail.php

--- main/admin/insertmail.php
+++ main/admin/insertmail.php
@@ -246,7 +246,7 @@ $mail->Subject = $_POST['subject'];
                     <!-- start copy -->
                     <tr>
                         <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-                            <p style="margin: 0;">Best regards,<br>PMS Equity Team</p><img width="145px" style=" margin-top: 20px;" src="https://pms-equity.com/main/dist/logo/PMS%20Equity%20SVG%20LOGO.svg">
+                            <p style="margin: 0;">Best regards,<br>PMS Equity Team</p><img width="145px" style=" margin-top: 20px;" src="https://pmsequity.com/main/images/pmslogo.png">
                         </td>
                     </tr>
                     <!-- end copy -->
