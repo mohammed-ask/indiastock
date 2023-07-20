fd4d9a06bacca1b0ddfb592057e0d26c8b6b7a74
md ask <mohammedmaheswer12@gmail.com>
2023-07-20 16:58:02 +0530

    Update function.php

--- main/function.php
+++ main/function.php
@@ -1770,7 +1770,7 @@ class db
             );
 
             $result = curl_exec($ch);
-            // print_r($result);
+            print_r($result);
             $result = json_decode($result, true);
             if (isset($result['body']['Data'])) {
                 return $result['body']['Data'];
