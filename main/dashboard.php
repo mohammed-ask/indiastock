279b52b376c9fcb3e4d6b2b6ba7822dce7266ae7
akshatgroup <marketing.akshatgroup@gmail.com>
2023-09-10 11:55:28 +0530

    some changes

--- main/dashboard.php
+++ main/dashboard.php
@@ -39,7 +39,7 @@ if ($dashboardmaintanance) {
             <div class="row">
                 <div class="col-sm-12 mt-2 mb-2">
                     <div class="page-title-box d-inline-block d-md-flex justify-content-start justify-content-md-between align-items-center">
-                        <div class="my-3 my-md-0 ps-2 m-subheader">
+                        <div class="my-md-0 m-subheader">
                             <?php foreach ($marketdata as $mdata) {  ?>
                                 <div class="nifty-50 d-inline-block me-3 nifty-sensex-border">
                                     <div class="font-11 fw-semibold"><?= $mdata['Symbol'] ?></div>
--- main/getlivemarketdatadashboard.php
+++ main/getlivemarketdatadashboard.php
@@ -24,7 +24,7 @@ $wstocks = array_filter($stockdata, function ($data) {
             <div class="page-title-box d-inline-block d-md-flex justify-content-start justify-content-md-between align-items-center">
                 <div class="my-3 my-md-0 ps-2">
                     <?php foreach ($marketdata as $mdata) {  ?>
-                        <div class="nifty-50 d-inline-block me-3">
+                        <div class="nifty-50 d-inline-block">
                             <div class="font-11 fw-semibold"><?= $mdata['Symbol'] ?></div>
                             <div class="d-inline-block font-11"><?= $mdata['LastRate'] ?> <span <?= $mdata['ChgPcnt'] > 0 ? "class='text-success'" : "class='text-danger'" ?>><?= $mdata['Chg'] ?> </span>
                                 <span <?= $mdata['ChgPcnt'] > 0 ? "class='text-success'" : "class='text-danger'" ?>>(<?= round($mdata['ChgPcnt'], 2) ?>%)</span>
