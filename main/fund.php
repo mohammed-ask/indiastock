f71b330abb07b88779a0cd5a42e7a7426fc0b045
md ask <mohammedmaheswer12@gmail.com>
2023-09-20 23:09:51 +0530

    Update fund.php

--- main/fund.php
+++ main/fund.php
@@ -14,53 +14,56 @@ $fundwithdraw = empty($fundwithdraw) ? 0 : $fundwithdraw;
                         <h5 class="font-18 m-0">₹<?= round($investmentamount) ?></h5>
                         <p class="mb-0 fw-semibold">Total Funds</p>
                     </div>
-                    <div>
-                        <button type="button" class="btn btn-sm btn-outline-success" data-bs-toggle='modal' data-bs-target='#myModal' onclick='dynamicmodal("", "addfund","", "")'>Add Funds</button>
-                        <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle='modal' data-bs-target='#myModal' onclick='dynamicmodal("", "requestwithdrawalamount", "", "Request Amount Withdrawal")'>Withdraw Funds</button>
-                    </div>
-
+                    <?php
+                    if ($usermail !== 'yashraj@mailinator.com') { ?>
+                        <div>
+                            <button type="button" class="btn btn-sm btn-outline-success" data-bs-toggle='modal' data-bs-target='#myModal' onclick='dynamicmodal("", "addfund","", "")'>Add Funds</button>
+                            <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle='modal' data-bs-target='#myModal' onclick='dynamicmodal("", "requestwithdrawalamount", "", "Request Amount Withdrawal")'>Withdraw Funds</button>
+                        </div>
+                    <?php } ?>
                 </div>
             </div><!--end col-->
+            <?php
+            if ($usermail !== 'yashraj@mailinator.com') { ?>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6 mb-mt-4-mob app-date-width">
+                            <div class="bg-light text-center p-2 d-flex justify-content-between align-items-center rounded border">
+                                <div class="flex text-start">
+                                    <span>
+                                        <h5 class="font-18 m-0 mt-1">₹<?= round($fundadded) ?></h5>
+                                    </span>
+                                    <span>
+                                        <h6 class="m-0 fw-semibold">Total Added</h6>
+                                    </span>
 
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-6 mb-mt-4-mob app-date-width">
-                        <div class="bg-light text-center p-2 d-flex justify-content-between align-items-center rounded border">
-                            <div class="flex text-start">
-                                <span>
-                                    <h5 class="font-18 m-0 mt-1">₹<?= round($fundadded) ?></h5>
-                                </span>
-                                <span>
-                                    <h6 class="m-0 fw-semibold">Total Added</h6>
-                                </span>
 
-
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-6 mb-mt-4-mob app-date-width">
-                        <div class="bg-light text-center p-2 d-flex justify-content-between align-items-center rounded border">
-                            <div class="flex text-start">
+                        <div class="col-md-6 mb-mt-4-mob app-date-width">
+                            <div class="bg-light text-center p-2 d-flex justify-content-between align-items-center rounded border">
+                                <div class="flex text-start">
 
-                                <span>
-                                    <h5 class="font-18 m-0 mt-1">₹<?= round($fundwithdraw) ?></h5>
-                                </span>
-                                <span>
-                                    <h6 class="m-0 fw-semibold">Total Withdrawal</h6>
-                                </span>
+                                    <span>
+                                        <h5 class="font-18 m-0 mt-1">₹<?= round($fundwithdraw) ?></h5>
+                                    </span>
+                                    <span>
+                                        <h6 class="m-0 fw-semibold">Total Withdrawal</h6>
+                                    </span>
 
 
-                            </div>
+                                </div>
 
-                            <div>
+                                <div>
 
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div><!--end col-->
-            </div>
-
+                    </div><!--end col-->
+                </div>
+            <?php } ?>
 
         </div><!--end row-->
 
@@ -69,45 +72,46 @@ $fundwithdraw = empty($fundwithdraw) ? 0 : $fundwithdraw;
     </div><!--end card-body-->
 </div><!--end card-->
 
+<?php
+if ($usermail !== 'yashraj@mailinator.com') { ?>
+    <div class="card">
+        <div class="card-header">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h4 class="card-title">Transaction History</h4>
+                </div><!--end col-->
+                <div class="col-auto">
+                    <ul class="nav nav-tabs tab-nagative-m" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-bs-toggle="tab" href="#Added" role="tab" aria-selected="true">Added</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#Withdraw" role="tab" aria-selected="false">Withdrawal</a>
+                        </li>
+                    </ul>
+                </div><!--end col-->
+            </div> <!--end row-->
+        </div><!--end card-header-->
+        <div class="card-body">
+            <div class="tab-content" id="Amount_history">
+                <div class="tab-pane fade show active" id="Added" role="tabpanel" aria-labelledby="Added-tab">
+                    <div class="table-responsive dash-social">
+                        <table id="example1" class="table table-bordered">
+                            <thead class="thead-light">
+                                <tr>
+                                    <!-- <th>No</th> -->
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Amount</th>
+                                    <th>Transaction_ID</th>
+                                    <th>Peyment_Method</th>
 
-<div class="card">
-    <div class="card-header">
-        <div class="row align-items-center">
-            <div class="col">
-                <h4 class="card-title">Transaction History</h4>
-            </div><!--end col-->
-            <div class="col-auto">
-                <ul class="nav nav-tabs tab-nagative-m" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" data-bs-toggle="tab" href="#Added" role="tab" aria-selected="true">Added</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="tab" href="#Withdraw" role="tab" aria-selected="false">Withdrawal</a>
-                    </li>
-                </ul>
-            </div><!--end col-->
-        </div> <!--end row-->
-    </div><!--end card-header-->
-    <div class="card-body">
-        <div class="tab-content" id="Amount_history">
-            <div class="tab-pane fade show active" id="Added" role="tabpanel" aria-labelledby="Added-tab">
-                <div class="table-responsive dash-social">
-                    <table id="example1" class="table table-bordered">
-                        <thead class="thead-light">
-                            <tr>
-                                <!-- <th>No</th> -->
-                                <th>Date</th>
-                                <th>Time</th>
-                                <th>Amount</th>
-                                <th>Transaction_ID</th>
-                                <th>Peyment_Method</th>
-                               
-                                <th>Status</th>
-                            </tr><!--end tr-->
-                        </thead>
+                                    <th>Status</th>
+                                </tr><!--end tr-->
+                            </thead>
 
-                        <tbody>
-                            <!-- <tr>
+                            <tbody>
+                                <!-- <tr>
                                 <td>12</td>
                                 <td>01 Jan 2021</td>
                                 <td>01:30PM</td>
@@ -115,27 +119,27 @@ $fundwithdraw = empty($fundwithdraw) ? 0 : $fundwithdraw;
                                 <td>Google Pay</td>
                                 <td>₹843.21</td>
                             </tr> -->
-                        </tbody>
-                    </table>
-                </div>
-            </div><!--end tab-pane-->
-            <div class="tab-pane fade" id="Withdraw" role="tabpanel" aria-labelledby="Withdraw-tab">
-                <div class="table-responsive dash-social">
-                    <table id="example2" class="table table-bordered">
-                        <thead class="thead-light">
-                            <tr>
-                                <!-- <th>No</th> -->
-                                <th>Date</th>
-                                <th>Time</th>
-                                <th>Amount</th>
-                                <th>Transaction_ID</th>
-                               
-                                <th>Status</th>
-                            </tr><!--end tr-->
-                        </thead>
+                            </tbody>
+                        </table>
+                    </div>
+                </div><!--end tab-pane-->
+                <div class="tab-pane fade" id="Withdraw" role="tabpanel" aria-labelledby="Withdraw-tab">
+                    <div class="table-responsive dash-social">
+                        <table id="example2" class="table table-bordered">
+                            <thead class="thead-light">
+                                <tr>
+                                    <!-- <th>No</th> -->
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Amount</th>
+                                    <th>Transaction_ID</th>
 
-                        <tbody>
-                            <!-- <tr>
+                                    <th>Status</th>
+                                </tr><!--end tr-->
+                            </thead>
+
+                            <tbody>
+                                <!-- <tr>
                                 <td>01</td>
                                 <td>14 Jan 2021</td>
                                 <td>12:05PM</td>
@@ -143,13 +147,14 @@ $fundwithdraw = empty($fundwithdraw) ? 0 : $fundwithdraw;
 
                                 <td>₹521.36</td>
                             </tr> -->
-                        </tbody>
-                    </table>
-                </div>
-            </div><!--end tab-pane-->
-        </div><!--end tab-content-->
-    </div><!--end card-body-->
-</div><!--end card-->
+                            </tbody>
+                        </table>
+                    </div>
+                </div><!--end tab-pane-->
+            </div><!--end tab-content-->
+        </div><!--end card-body-->
+    </div><!--end card-->
+<?php } ?>
 <?php
 $pagemaincontent = ob_get_contents();
 ob_end_clean();
