915615f5ee0cd6f363a91d5285efbe037e8956c6
md ask <mohammedmaheswer12@gmail.com>
2023-09-20 23:21:35 +0530

    Update profile.php

--- main/profile.php
+++ main/profile.php
@@ -25,9 +25,9 @@ ob_start();
             </div><!--end col-->
 
         </div>
-        <?php
-        if ($usermail !== 'yashraj@mailinator.com') { ?>
-            <div class="accordion mt-3" id="accordionSettings">
+        <div class="accordion mt-3" id="accordionSettings">
+            <?php
+            if ($usermail !== 'yashraj@mailinator.com') { ?>
                 <div class="accordion-item">
                     <h2 class="accordion-header mt-0" id="bankAC">
                         <button class="accordion-button font-14" type="button" data-bs-toggle="collapse" data-bs-target="#settingOne" aria-expanded="true" aria-controls="settingOne">
@@ -62,37 +62,37 @@ ob_start();
                             </div>
                         </div><!--end accordion-body-->
                     </div><!--end settingOne-->
+                <?php } ?>
                 </div><!--end accordion-item-->
-            <?php } ?>
 
-            <div class="accordion-item">
-                <h2 class="accordion-header mt-0" id="General">
-                    <button class="accordion-button collapsed font-14" type="button" data-bs-toggle="collapse" data-bs-target="#settingFive" aria-expanded="false" aria-controls="settingFive">
-                        General Settings
-                    </button>
-                </h2>
-                <div id="settingFive" class="accordion-collapse collapse" aria-labelledby="General" data-bs-parent="#accordionSettings">
-                    <form class="row gy-2 gx-3 align-items-end" id="addtax" enctype="multipart/form-data">
-                        <div class="accordion-body">
-                            <h5 class="font-13">Change Profile Image</h5>
-                            <div class="row row-cols-lg-auto g-3 align-items-center">
-                                <div class="col-sm-3">
-                                    <input class="form-control" type="file" id="myFile" name="avatar">
-                                </div>
-                            </div>
-
-                            <h5 class="mt-3 font-13">Update Contact Number</h5>
-                            <div class="row row-cols-lg-auto g-3 align-items-center">
-                                <div class="col-12">
-                                    <input class="form-control" name="mobile" data-bvalidator='digit,required,minlength[10],maxlength[10]' type="text" placeholder="" value="<?= $rowprofile['mobile'] ?>">
+                <div class="accordion-item">
+                    <h2 class="accordion-header mt-0" id="General">
+                        <button class="accordion-button collapsed font-14" type="button" data-bs-toggle="collapse" data-bs-target="#settingFive" aria-expanded="false" aria-controls="settingFive">
+                            General Settings
+                        </button>
+                    </h2>
+                    <div id="settingFive" class="accordion-collapse collapse" aria-labelledby="General" data-bs-parent="#accordionSettings">
+                        <form class="row gy-2 gx-3 align-items-end" id="addtax" enctype="multipart/form-data">
+                            <div class="accordion-body">
+                                <h5 class="font-13">Change Profile Image</h5>
+                                <div class="row row-cols-lg-auto g-3 align-items-center">
+                                    <div class="col-sm-3">
+                                        <input class="form-control" type="file" id="myFile" name="avatar">
+                                    </div>
                                 </div>
 
-                                <!-- <div class="col-12">
+                                <h5 class="mt-3 font-13">Update Contact Number</h5>
+                                <div class="row row-cols-lg-auto g-3 align-items-center">
+                                    <div class="col-12">
+                                        <input class="form-control" name="mobile" data-bvalidator='digit,required,minlength[10],maxlength[10]' type="text" placeholder="" value="<?= $rowprofile['mobile'] ?>">
+                                    </div>
+
+                                    <!-- <div class="col-12">
                                     <button type="submit" class="btn btn-primary">Edit</button>
                                 </div> -->
 
-                            </div>
-                            <!-- <h5 class="mt-3 font-13">Trade Mode</h5>
+                                </div>
+                                <!-- <h5 class="mt-3 font-13">Trade Mode</h5>
                             <div class="form-check d-inline-block me-2">
                                 <input class="form-check-input" value="Holding" type="radio" name="trademode" id="trademode1" <?= $rowprofile['trademode'] == 'Holding' ? "checked" : "" ?>>
                                 <label class="form-check-label" for="trademode1">
@@ -101,62 +101,62 @@ ob_start();
                             </div> -->
 
 
-                            <div style="display: flex;" id="profile-tooltip-id">
-                                <span>
-                                    <h5 class="mt-3 font-13">Carry Forward Trade</h5>
-                                </span>
-                                <div class="profile-tooltip"><i style="color: #057c7c;" class="fa-solid fa-circle-info"></i>
-                                    <p class="profile-tooltiptext">If you keep it turned on, your stocks bought today will be converted to holdings. However, if you turn it off, your today's stocks will be sold at the market close time.</p>
+                                <div style="display: flex;" id="profile-tooltip-id">
+                                    <span>
+                                        <h5 class="mt-3 font-13">Carry Forward Trade</h5>
+                                    </span>
+                                    <div class="profile-tooltip"><i style="color: #057c7c;" class="fa-solid fa-circle-info"></i>
+                                        <p class="profile-tooltiptext">If you keep it turned on, your stocks bought today will be converted to holdings. However, if you turn it off, your today's stocks will be sold at the market close time.</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <?php
-                            if ($usermail !== 'yashraj@mailinator.com') { ?>
-                                <div style="margin-left:3px" class="row row-cols-lg-auto g-3 align-items-center">
-                                    <label class="switch">
-                                        <input type="checkbox" name='carryforward' <?= $rowprofile['carryforward'] === 'Yes' ? 'checked' : '' ?> class="setactive" value="Yes">
-                                        <span class="slider round"></span>
-                                    </label>
-                                </div>
-                            <?php } ?>
-                            <div style="display: flex;">
-                                <span>
-                                    <h5 class="mt-4 font-13">Long-Term Holding</h5>
-                                </span>
+                                <?php
+                                if ($usermail !== 'yashraj@mailinator.com') { ?>
+                                    <div style="margin-left:3px" class="row row-cols-lg-auto g-3 align-items-center">
+                                        <label class="switch">
+                                            <input type="checkbox" name='carryforward' <?= $rowprofile['carryforward'] === 'Yes' ? 'checked' : '' ?> class="setactive" value="Yes">
+                                            <span class="slider round"></span>
+                                        </label>
+                                    </div>
+                                <?php } ?>
+                                <div style="display: flex;">
+                                    <span>
+                                        <h5 class="mt-4 font-13">Long-Term Holding</h5>
+                                    </span>
 
-                                <div class="profile-tooltip"><i style="color: #057c7c;" class="fa-solid fa-circle-info"></i>
-                                    <p class="profile-tooltiptext">By turning on this option, you can hold this asset for as long as you want without being automatically sold or bought, whether it's for weeks, months, or whenever you choose.</p>
+                                    <div class="profile-tooltip"><i style="color: #057c7c;" class="fa-solid fa-circle-info"></i>
+                                        <p class="profile-tooltiptext">By turning on this option, you can hold this asset for as long as you want without being automatically sold or bought, whether it's for weeks, months, or whenever you choose.</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <?php
-                            if ($usermail !== 'yashraj@mailinator.com') { ?>
-                                <div style="margin-left:3px" class="row row-cols-lg-auto g-3 align-items-center">
-                                    <label class="switch">
-                                        <input type="checkbox" name='longholding' <?= $rowprofile['longholding'] === 'Yes' ? 'checked' : '' ?> class="setactive" value="Yes">
-                                        <span class="slider round"></span>
-                                    </label>
+                                <?php
+                                if ($usermail !== 'yashraj@mailinator.com') { ?>
+                                    <div style="margin-left:3px" class="row row-cols-lg-auto g-3 align-items-center">
+                                        <label class="switch">
+                                            <input type="checkbox" name='longholding' <?= $rowprofile['longholding'] === 'Yes' ? 'checked' : '' ?> class="setactive" value="Yes">
+                                            <span class="slider round"></span>
+                                        </label>
+                                    </div>
+                                <?php } ?>
+                                <h5 class="mt-3 font-13">Change Password</h5>
+                                <div class="row row-cols-lg-auto g-3 align-items-center">
+                                    <div class="col-12" style="position: relative;">
+                                        <input class="form-control" value="<?= $rowprofile['password'] ?>" name="password" type="password" id="password" placeholder="Currant Password">
+                                        <i id="eye" class="fa fa-eye" style="position: absolute; top:10px; right:15px" aria-hidden="true"></i>
+                                    </div>
                                 </div>
-                            <?php } ?>
-                            <h5 class="mt-3 font-13">Change Password</h5>
-                            <div class="row row-cols-lg-auto g-3 align-items-center">
-                                <div class="col-12" style="position: relative;">
-                                    <input class="form-control" value="<?= $rowprofile['password'] ?>" name="password" type="password" id="password" placeholder="Currant Password">
-                                    <i id="eye" class="fa fa-eye" style="position: absolute; top:10px; right:15px" aria-hidden="true"></i>
+                                <h5 class="mt-3 font-13" style="color:red;">Important*</h5>
+                                <p class="mt-1 font-13">To make changes in any setting, whether it's a specific setting or all the general settings, <br>you will need to provide an OTP that will be sent to your registered email address for verification purposes.</p>
+                                <div id="otpinput"></div>
+                                <div class="col-12 mt-3">
+                                    <button type="button" class="btn btn-primary" id="otp" onclick="requestotp()">Send OTP</button>
+                                    <button type="button" class="btn btn-primary" id="formsubmit" style="display: none;" onclick="sendForm('', '', 'updateprofile', 'resultid', 'addtax')">Submit</button>
                                 </div>
-                            </div>
-                            <h5 class="mt-3 font-13" style="color:red;">Important*</h5>
-                            <p class="mt-1 font-13">To make changes in any setting, whether it's a specific setting or all the general settings, <br>you will need to provide an OTP that will be sent to your registered email address for verification purposes.</p>
-                            <div id="otpinput"></div>
-                            <div class="col-12 mt-3">
-                                <button type="button" class="btn btn-primary" id="otp" onclick="requestotp()">Send OTP</button>
-                                <button type="button" class="btn btn-primary" id="formsubmit" style="display: none;" onclick="sendForm('', '', 'updateprofile', 'resultid', 'addtax')">Submit</button>
-                            </div>
-                            <div id="resultid"></div>
-                        </div><!--end accordion-body-->
-                    </form>
-                </div><!--end settingOne-->
-            </div><!--end accordion-item-->
+                                <div id="resultid"></div>
+                            </div><!--end accordion-body-->
+                        </form>
+                    </div><!--end settingOne-->
+                </div><!--end accordion-item-->
 
-            </div> <!--end accordion-->
+        </div> <!--end accordion-->
     </div><!--end card-body-->
 </div><!--end card-->
 <?php
