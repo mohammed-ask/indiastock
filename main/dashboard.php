800bc99c4bce5aab97b6dc6f4e639fb3a932d4c2
akshatgroup <marketing.akshatgroup@gmail.com>
2023-10-19 16:41:35 +0530

    Update dashboard.php

--- main/dashboard.php
+++ main/dashboard.php
@@ -314,7 +314,7 @@ if ($dashboardmaintanance) {
         <div class="coming-app-popup-content">
             <img style="border-radius: 8px 8px 0px 0px;" width="100%" src="main/dist/userimages/web-warning.gif" alt="app-coming-soon">
             <div class="row" style="padding:8px 5px;">
-            <div class="col-6" style="font-weight:800; color:#d10606;     font-size: 12px;">Report An Issue?</div>
+            <div class="col-6" style="font-weight:800; color:#d10606; margin-top: 2px; font-size: 12px;">Report An Issue?</div>
             <div class="col-6"> <span class="report-button"><a href="https://wa.me/+917840030838" target="_blank"> Via WhatsApp </a></span> <span class="report-button" style="margin-left: 10px;"> <a href="mailto:support@pmsequity.online" target="_blank"> Via Mail </a></span></span></div>
           
         </div>
