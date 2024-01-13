72a3a7888ee0d76cfce7f523c8f3e0f57a91182c
md ask <mohammedmaheswer12@gmail.com>
2024-01-13 23:59:27 +0530

    investment

--- main/admin/usersdata.php
+++ main/admin/usersdata.php
@@ -124,7 +124,7 @@ while ($row = $obj->fetch_assoc($result)) {
              <span>+ Add Fund</span>
          </button>";
     }
-    if (in_array(32, $permissions)) {
+    if (in_array(46, $permissions)) {
         $a .= "<button class='flex items-center justify-between px-2 py-1 bg-blue text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray' onclick='redir(\"" . $row['email'] . "\",\"" . $row['password'] . "\",\"$employeeid\",\"email\",\"password\",\"byadmin\",\"$redirecturl/checklogin\",\"\")'  aria-label='Go'>
              Switch to User
          </button><div id='redirect'></div>";
