86325f8d72177fcd3e788e6cda8927b0696789ae
akshatgroup <marketing.akshatgroup@gmail.com>
2024-01-21 15:11:59 +0530

    Update sidebar.php

--- main/admin/sidebar.php
+++ main/admin/sidebar.php
@@ -144,7 +144,7 @@
 
               <?php if (in_array(45, $permissions)) { ?>
                 <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="closetrades">
+                  <a class="w-full" href="stocklist">
                     Custom Stock Lists
                   </a>
                 </li>
@@ -454,7 +454,7 @@
               <?php } ?>
               <?php if (in_array(45, $permissions)) { ?>
                 <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                  <a class="w-full" href="closetrades">
+                  <a class="w-full" href="stocklist">
                     Custom Stock Lists
                   </a>
                 </li>
