f4788994d7c65473fd5ce79930e28c7d54570ca4
akshatgroup <marketing.akshatgroup@gmail.com>
2024-01-28 11:22:23 +0530

    Update showscreenshot.php

--- main/admin/showscreenshot.php
+++ main/admin/showscreenshot.php
@@ -3,7 +3,7 @@ include "main/session.php";
 $pmid = $_GET['hakuna'];
 
 ?>
-<img style="height:450px;width:510px" src="../<?= $obj->fetchattachment($pmid) ?>" />
+<img src="../<?= $obj->fetchattachment($pmid) ?>" />
 <script>
     $("#modalfooterbtn").css('display', 'none')
 </script>
