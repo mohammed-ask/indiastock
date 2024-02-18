005ea9c1439367a08fb99f0e8ceb2c76a3c5b939
md ask <mohammedmaheswer12@gmail.com>
2024-02-18 20:02:23 +0530

    Update getmpin.php

--- main/api/getmpin.php
+++ main/api/getmpin.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Asia/Kolkata');
 ini_set('memory_limit', '-1');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 header('Access-Control-Request-Headers: *');
